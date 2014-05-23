e0a6d608ceea59d09957ab55c665b5d794f1896e
Jon Allured <jon.allured@gmail.com>
2014-05-23 12:48:56 -0500

    Expose the raw response

--- lib/marvelite/api/response.rb
+++ lib/marvelite/api/response.rb
@@ -5,6 +5,15 @@ module Marvelite
       include Hashie::Extensions::KeyConversion
       include Hashie::Extensions::MethodAccess
       include Hashie::Extensions::IndifferentAccess
+
+      def initialize(raw)
+        @raw = raw
+        super(raw)
+      end
+
+      def raw_response
+        @raw.response
+      end
     end
 
     class ErrorResponse < Response; end
